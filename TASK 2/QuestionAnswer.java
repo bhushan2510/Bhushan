0164de403a9affc07b0a74583709d1ed03e1eacf
bhushan2510 <110326987+bhushan2510@users.noreply.github.com>
2022-08-07 14:03:53 +0530

    .
    The tasks that I have completed during my Android Application development Internship at Oasis Infobyte.

---
+++ TASK 2/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not programming language",
+            "where you are watching this video?",
+            "Which company owns the Apple?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+
+    };
+
+    public static String correctAnswer[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+}
